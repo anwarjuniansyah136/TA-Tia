b11466b24ca994e6c6c152a117bdc4d23d3b67af
Anwar Juniansyah Harahap <anwarjuniansyah136@gmail.com>
2026-08-03 22:53:31 +0700

    refactor:adjustment endpoint history

--- src/main/java/com/rental/Inventory/repository/ProductRepository.java
+++ src/main/java/com/rental/Inventory/repository/ProductRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface ProductRepository extends JpaRepository<Products, String> {
 
-    @Query("SELECT COALESCE(SUM(p.stock), 0) FROM Products p")
+    @Query("SELECT COALESCE(SUM(p.stock), 0) FROM Products p WHERE p.status = true")
     Long sumTotalStock();
 
 }
